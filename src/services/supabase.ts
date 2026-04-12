d15860388940d9e9c5b0cd9972780b41c1c3d207
marcusviniciusend <vivipebito@gmail.com>
2026-04-12 17:20:07 -0300

    security: dotenv e chaves do supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,5 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
-const supabaseUrl = 'https://njbqzqjweuzirctczktv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qYnF6cWp3ZXV6aXJjdGN6a3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MjQ2NTgsImV4cCI6MjA5MTUwMDY1OH0.QL-6NzombkhML5Ibx9Zje003-BtarVoslHPTEaVkmuM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
